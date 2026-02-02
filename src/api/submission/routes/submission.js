065d7fdea12d2c08f7f235bd15a82e416bb52ab9
sophia860 <sophia@pagegalleryjournal.com>
2026-02-02 15:24:39 -0500

    Fix submission routes to use standard core router
    Removed custom submission routes and exported only the default router.

--- src/api/submission/routes/submission.js
+++ src/api/submission/routes/submission.js
@@ -6,48 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-const defaultRouter = createCoreRouter('api::submission.submission');
-
-// Custom routes for submission workflow
-const customRoutes = [
-  {
-    method: 'POST',
-    path: '/submissions/:id/submit',
-    handler: 'submission.submitForReview',
-    config: {
-      policies: [],
-      middlewares: [],
-    },
-  },
-  {
-    method: 'POST',
-    path: '/submissions/:id/accept',
-    handler: 'submission.accept',
-    config: {
-      policies: [],
-      middlewares: [],
-    },
-  },
-  {
-    method: 'POST',
-    path: '/submissions/:id/reject',
-    handler: 'submission.reject',
-    config: {
-      policies: [],
-      middlewares: [],
-    },
-  },
-  {
-    method: 'POST',
-    path: '/submissions/:id/request-revision',
-    handler: 'submission.requestRevision',
-    config: {
-      policies: [],
-      middlewares: [],
-    },
-  },
-];
-
-module.exports = {
-  routes: [...customRoutes, ...defaultRouter.routes],
-};
+module.exports = createCoreRouter('api::submission.submission');
